16a61a46efb4b98ef4025fcdb7df7d4b4c34cc6c
rodrixin234 <hurtarterod3107@gmail.com>
2024-10-25 13:05:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function buscarJoyas() {
+    const searchInput = document.getElementById("busqueda").value;
+    alert(`Buscando: ${searchInput}`);
+}
+
+function mostrarCategoria(categoria) {
+    alert(`Mostrando la categoría: ${categoria}`);
+}
